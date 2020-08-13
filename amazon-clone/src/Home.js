dbe97313c9016cdd3ba8792156490f1479d22a33
sapirdeu <sapiroosh.d@gmail.com>
2020-08-13 21:27:32 +0300

    add products

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -53,7 +53,7 @@ function Home() {
                     id="12321346"
                     title="Samsung LC49RG90SSUXEN 49 Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
                     price={1094.98}
-                    rating={4}
+                    rating={3}
                     image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SL1000_.jpg"
                 />
             </div>
