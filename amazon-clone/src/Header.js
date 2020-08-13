803488e9a30813582014b9d8262b32f8c1bd4bb1
sapirdeu <sapiroosh.d@gmail.com>
2020-08-13 14:05:57 +0300

    React Context + Add to Basket

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -3,8 +3,13 @@ import './Header.css'
 import {Link} from "react-router-dom";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppindBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{basket}] = useStateValue();
+
+    console.log(basket);
+
     return (
         <nav className="header">
             {/* Logo on the left -> img */}
@@ -50,7 +55,7 @@ function Header() {
                         {/* Shopping basket icon */}
                         <ShoppindBasketIcon/>
                         {/* Number of items in the basket */}
-                        <span className="header__optionLineTwo header__basketCount">0</span>
+                        <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
                     </div>
             </Link>
 
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 function Product({id, title, price, rating, image}) {
+    const [{}, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="product__info">
@@ -20,7 +36,7 @@ function Product({id, title, price, rating, image}) {
             </div>
             
             <img src={image} alt=""/>
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
         </div>
     )
 }
